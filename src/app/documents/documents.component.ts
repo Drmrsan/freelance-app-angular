97d5c56b49b95a9583bb6765065baa6c6fc105a5
Drmrsan <drmrsan@gmail.com>
2017-02-05 22:44:49 +0100

    Hard coding data for Documents

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document'
 
 @Component({
 	moduleId: module.id,
@@ -7,5 +8,30 @@ import { Component } from '@angular/core';
 })
 
 export class DocumentsComponent {
-	
+	pageTitle: string = "Document Dashboard"
+
+	documents: Document[] = [
+		{
+			title: 'My First doc',
+			description: 'some description',
+			file_url: 'http://google.com',
+			updated_at: '5/2/2017',
+			image_url: 'http://google.com',
+		},
+		{
+			title: 'My Second doc',
+			description: 'some description',
+			file_url: 'http://google.com',
+			updated_at: '5/2/2017',
+			image_url: 'http://google.com',
+		},
+		{
+			title: 'My Third doc',
+			description: 'some description',
+			file_url: 'http://google.com',
+			updated_at: '5/2/2017',
+			image_url: 'http://google.com',
+		}
+
+	]
 }
